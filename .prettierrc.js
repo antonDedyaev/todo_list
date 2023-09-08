01ad2658a8c3e7f4ff54b47b34c30d07fd674dbd
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-09-08 16:02:06 +0300

    installed and setup eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  printWidth: 150,
+};
